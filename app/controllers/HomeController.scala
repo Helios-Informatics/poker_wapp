c0899f8be8aa3e1c56c4712353ab97b3aa26aff5
Radon-css <Julianschmuck@outlook.de>
2024-05-08 20:39:39 +0200

    Defined First routes(Errors)

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,20 +4,24 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import de.htwg.poker.util.Evaluator
+import de.htwg.poker.controller.Controller
 
 /** This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
   */
 @Singleton
-class HomeController @Inject() (val controllerComponents: ControllerComponents)
-    extends BaseController {
+class HomeController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
 
-  /** Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method will be
-    * called when the application receives a `GET` request with a path of `/`.
-    */
+  val gameController = new Controller()
+
+
   def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+
+  def newGame() = Action { implicit request: Request[AnyContent] =>
+    val players = List("Player1", "Player2", "Player3", "Player4")
+    gameController.createGame(players,10,20)
     Ok(views.html.index())
   }
 }
