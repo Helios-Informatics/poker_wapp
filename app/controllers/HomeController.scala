8259cd35521a7eb67555475ba8b805c171d167ba
Radon-css <Julianschmuck@outlook.de>
2024-05-08 21:03:18 +0200

    First route working

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,6 +5,7 @@ import play.api._
 import play.api.mvc._
 import de.htwg.poker.util.Evaluator
 import de.htwg.poker.controller.Controller
+import de.htwg.poker.model.GameState
 
 /** This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
@@ -12,7 +13,9 @@ import de.htwg.poker.controller.Controller
 @Singleton
 class HomeController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
 
-  val gameController = new Controller()
+  val gameController = new Controller(
+    new GameState(Nil, None, None, 0, 0, Nil, 0, 0, 0, 0)
+  )
   def pokerAsText = gameController.toString()
 
 
@@ -22,7 +25,7 @@ class HomeController @Inject() (val controllerComponents: ControllerComponents) 
 
   def newGame() = Action { implicit request: Request[AnyContent] =>
     val players = List("Player1", "Player2", "Player3", "Player4")
-    gameController.createGame(players,10,20)
-    Ok(views.html.index(pokerAsText))
+    gameController.createGame(players,"10","20")
+    Ok(pokerAsText)
   }
 }
