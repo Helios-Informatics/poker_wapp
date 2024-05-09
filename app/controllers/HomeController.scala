e65e27d7d3cd0651d62452653f94fef8582cf9be
HenrikFrs <henrik.freisleben@gmail.com>
2024-05-09 21:43:47 +0200

    finish task 2

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,26 +11,52 @@ import de.htwg.poker.model.GameState
   * application's home page.
   */
 @Singleton
-class HomeController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (val controllerComponents: ControllerComponents)
+    extends BaseController {
 
   val gameController = new Controller(
     new GameState(Nil, None, None, 0, 0, Nil, 0, 0, 0, 0)
   )
   def pokerAsText = gameController.toString()
 
-
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 
   def newGame() = Action { implicit request: Request[AnyContent] =>
-    val players = List("Player1", "Player2", "Player3", "Player4")
-    gameController.createGame(players,"10","20")
+    val players =
+      List("Player1", "Player2", "Player3", "Player4", "Player5", "Player6")
+    gameController.createGame(players, "10", "20")
     Ok(pokerAsText)
   }
 
   def bet(amount: Int) = Action { implicit request: Request[AnyContent] =>
     gameController.bet(amount)
+    Ok(pokerAsText)
+  }
+
+  def allIn() = Action { implicit request: Request[AnyContent] =>
+    gameController.allIn()
+    Ok(pokerAsText)
+  }
+
+  def fold() = Action { implicit request: Request[AnyContent] =>
+    gameController.fold
+    Ok(pokerAsText)
+  }
+
+  def call() = Action { implicit request: Request[AnyContent] =>
+    gameController.call
+    Ok(pokerAsText)
+  }
+
+  def check() = Action { implicit request: Request[AnyContent] =>
+    gameController.check
+    Ok(pokerAsText)
+  }
+
+  def restartGame = Action { implicit request: Request[AnyContent] =>
+    gameController.restartGame
     Ok(pokerAsText)
   }
 }
