bd16136836c797c1868d999e656bab8267293761
HenrikFrs <henrik.freisleben@gmail.com>
2024-05-22 19:07:44 +0200

    started to interact with gui

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,4 @@
+// display slider value
 document.addEventListener('DOMContentLoaded', function () {
     var slider = document.getElementById('customRange3');
     var output = document.getElementById('sliderValue');
@@ -6,5 +7,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     slider.addEventListener('input', function () {
         output.innerText = "$ " + slider.value;
+    });
+});
+
+//call functions on button clicks
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('callButton').addEventListener('click', function () {
+        window.location.href = 'http://localhost:9000/call';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('foldButton').addEventListener('click', function () {
+        window.location.href = 'http://localhost:9000/fold';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('raiseButton').addEventListener('click', function () {
+        var amount = document.getElementById('customRange3').value
+        window.location.href = 'http://localhost:9000/bet/' + amount;
     });
 });
