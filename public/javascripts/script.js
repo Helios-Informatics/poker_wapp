3c43c64cd217c6f5db41cbfa9c32de9a4613e576
HenrikFrs <henrik.freisleben@gmail.com>
2024-05-26 21:50:32 +0200

    .

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,24 +8,26 @@ document.addEventListener('DOMContentLoaded', function () {
     slider.addEventListener('input', function () {
         output.innerText = "$ " + slider.value;
     });
-});
 
-//call functions on button clicks
-document.addEventListener('DOMContentLoaded', function () {
+
+    //call functions on button clicks
     document.getElementById('callButton').addEventListener('click', function () {
+        animateRing();
         window.location.href = 'http://localhost:9000/call';
     });
-});
 
-document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('foldButton').addEventListener('click', function () {
         window.location.href = 'http://localhost:9000/fold';
     });
-});
 
-document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('raiseButton').addEventListener('click', function () {
         var amount = document.getElementById('customRange3').value
         window.location.href = 'http://localhost:9000/bet/' + amount;
     });
+
+    document.getElementById('checkButton').addEventListener('click', function () {
+        window.location.href = 'http://localhost:9000/check';
+    });
+
+    
 });
