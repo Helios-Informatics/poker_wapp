ea684daaa1ad8c5585366fc09fcbfd45312ad437
Radon-css <Julianschmuck@outlook.de>
2024-11-17 22:20:52 +0100

    FIXED: some allignment issues

--- views/PokerView.vue
+++ views/PokerView.vue
@@ -63,11 +63,13 @@ function handleAction(action) {
 </script>
 
 <template>
-  <div class="d-flex w-100 h-100 body" v-if="gameState !== {}">
-    <div class="d-flex flex-column justify-space-evenly container-fluid w-100">
+  <div class="d-flex w-100 body" v-if="gameState !== {}">
+    <div
+      class="d-flex flex-column container-fluid w-100 h-100 align-center justify-space-evenly"
+    >
       <!-- Current Hand Info & Leave Button -->
       <div class="container-fluid w-100">
-        <div class="d-flex align-center justify-space-around row">
+        <div class="d-flex align-center justify-space-between">
           <div class="col text-white">Your current hand: {{ "handInfo" }}</div>
           <div class="col d-flex justify-end">
             <v-btn
@@ -83,20 +85,18 @@ function handleAction(action) {
 
       <!-- Players and Board Layout -->
       <div class="d-flex flex-column">
-        <div
-          class="d-flex flex-row justify-center align-center mb-2"
-          v-for="player in indexedPlayers.slice(0, 2)"
-          :key="player.index"
-        >
-          <Player
-            :id="'player-' + player.index"
-            :name="player.player.playername"
-            :balance="player.player.balance"
-            :folded="player.player.folded"
-            :position="
-              player.index === 0 ? 'top-left-player' : 'top-right-player'
-            "
-          />
+        <div class="d-flex justify-center align-center mb-2">
+          <div v-for="player in indexedPlayers.slice(0, 2)" :key="player.index">
+            <Player
+              :id="'player-' + player.index"
+              :name="player.player.playername"
+              :balance="player.player.balance"
+              :folded="player.player.folded"
+              :position="
+                player.index === 0 ? 'top-left-player' : 'top-right-player'
+              "
+            />
+          </div>
         </div>
 
         <div class="d-flex justify-center align-center">
@@ -159,9 +159,9 @@ function handleAction(action) {
           <!-- Right Player -->
           <div v-if="indexedPlayers.length >= 3" id="player-2">
             <Player
-              :name="indexedPlayers[2].playername"
-              :balance="indexedPlayers[2].balance"
-              :folded="indexedPlayers[2].folded"
+              :name="indexedPlayers[2].player.playername"
+              :balance="indexedPlayers[2].player.balance"
+              :folded="indexedPlayers[2].player.folded"
               position="right-player"
             />
           </div>
@@ -191,66 +191,63 @@ function handleAction(action) {
             />
           </div>
         </div>
-      </div>
 
-      <!-- Slider Value Display -->
-      <div class="container-fluid w-100">
+        <!-- Action Buttons -->
+        <div class="container-fluid w-100 mt-16">
+          <div class="d-flex align-center justify-end row">
+            <div class="col d-flex gap-3 align-end mr-3">
+              <v-btn
+                class="btn btn-danger rounded-pill responsive-button"
+                color="error"
+                @click="handleAction('fold')"
+                :disabled="!selfIsAtTurn"
+              >
+                <h5 class="mt-1 responsive-button-text">FOLD</h5>
+              </v-btn>
+
+              <v-btn
+                class="btn btn-success rounded-pill responsive-button"
+                color="success"
+                @click="handleAction('callCheck')"
+                :disabled="!selfIsAtTurn"
+              >
+                <h5 class="mt-1 responsive-button-text">
+                  {{ callCheckButtonText }}
+                </h5>
+              </v-btn>
+
+              <v-btn
+                class="btn btn-primary rounded-pill responsive-button"
+                color="primary"
+                @click="handleAction('raise')"
+                :disabled="!selfIsAtTurn"
+              >
+                <h5 class="mt-1 responsive-button-text">RAISE</h5>
+              </v-btn>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="d-flex flex-row justify-center align-end"
+      style="width: 100px; margin-bottom: 100px"
+    >
+      <v-slider
+        class="mr-5 responsive-slider-size"
+        v-model="sliderValue"
+        min="0"
+        max="1000"
+        step="10"
+        color="primary"
+        direction="vertical"
+      />
+      <div>
         <div
-          class="col d-flex justify-end text-white mr-4 mb-4 font fs-2"
+          class="col d-flex justify-end text-white mr-4 mb-4 font"
           id="sliderValue"
         >
           ${{ sliderValue }}
-        </div>
-      </div>
-
-      <!-- Action Buttons -->
-      <div class="container-fluid w-100">
-        <div class="d-flex align-center justify-end row">
-          <div class="col d-flex gap-3 justify-end mr-3">
-            <v-btn
-              class="btn btn-danger rounded-pill responsive-button"
-              color="error"
-              @click="handleAction('fold')"
-              :disabled="!selfIsAtTurn"
-            >
-              <h5 class="mt-1 responsive-button-text">FOLD</h5>
-            </v-btn>
-
-            <v-btn
-              class="btn btn-success rounded-pill responsive-button"
-              color="success"
-              @click="handleAction('callCheck')"
-              :disabled="!selfIsAtTurn"
-            >
-              <h5 class="mt-1 responsive-button-text">
-                {{ callCheckButtonText }}
-              </h5>
-            </v-btn>
-
-            <v-btn
-              class="btn btn-primary rounded-pill responsive-button"
-              color="primary"
-              @click="handleAction('raise')"
-              :disabled="!selfIsAtTurn"
-            >
-              <h5 class="mt-1 responsive-button-text">RAISE</h5>
-            </v-btn>
-
-            <div
-              class="d-flex flex-column justify-end align-center"
-              style="width: 100px"
-            >
-              <v-slider
-                class="mr-5 responsive-slider-size"
-                v-model="sliderValue"
-                min="0"
-                max="1000"
-                step="10"
-                color="primary"
-                direction="vertical"
-              />
-            </div>
-          </div>
         </div>
       </div>
     </div>
