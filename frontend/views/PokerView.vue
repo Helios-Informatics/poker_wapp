6f46910a864b5fdc9a47dbcd0fca85420df00689
HenrikFrs <141763686+HenrikFrs@users.noreply.github.com>
2025-01-12 14:23:13 +0100

    corrected player cards placement

--- frontend/views/PokerView.vue
+++ frontend/views/PokerView.vue
@@ -26,8 +26,6 @@ const indexedPlayers = computed(() =>
   gamestate.value.players.map((player, index) => ({ ...player, index }))
 );
 
-
-
 const playerAtTurn = computed(() => gamestate.value.playerAtTurn);
 const board = computed(() => gamestate.value.board);
 const pot = computed(() => gamestate.value.pot);
@@ -39,21 +37,18 @@ const selfIsAtTurn = computed(
     gamestate.value.players[gamestate.value.playerAtTurn]?.player.id
 );
 
-const handInfo = computed(
-  () =>
-  {
-    console.log("handInfo called")
-    for (const player of indexedPlayers.value) {
-      console.log("handInfo: checking Player: ", player)
-      if(getCookie("playerID") === player.player.id) {
-        console.log("found own player:", player.player.id)
-        console.log("handEval: ", player.player.handEval)
-        return player.player.handEval
-      }
+const handInfo = computed(() => {
+  console.log("handInfo called");
+  for (const player of indexedPlayers.value) {
+    console.log("handInfo: checking Player: ", player);
+    if (getCookie("playerID") === player.player.id) {
+      console.log("found own player:", player.player.id);
+      console.log("handEval: ", player.player.handEval);
+      return player.player.handEval;
     }
-    return "Error"
   }
-)
+  return "Error";
+});
 
 const callCheckButtonText = computed(() =>
   gamestate.value.players[gamestate.value.playerAtTurn]?.player
@@ -84,15 +79,25 @@ function handleAction(action) {
 
 <template>
   <div class="d-flex w-100 body" v-if="gameState !== {}">
-    <div class="d-flex flex-column container-fluid w-100 h-100 align-center justify-space-evenly">
+    <div
+      class="d-flex flex-column container-fluid w-100 h-100 align-center justify-space-evenly"
+    >
       <!-- Current Hand Info & Leave Button -->
       <div class="container-fluid w-100">
         <div class="d-flex align-center justify-space-between">
           <div class="col text-white">Your current hand: {{ handInfo }}</div>
           <div class="col d-flex justify-end">
-            <v-btn class="responsive-button mr-4 font-weight-bold text-none text-h6" color="grey-darken-3" rounded
-              variant="flat" text="Leave" height="50" prepend-icon="mdi-arrow-collapse-left"
-              @click="handleAction('leave')" :disabled="!selfIsAtTurn">
+            <v-btn
+              class="responsive-button mr-4 font-weight-bold text-none text-h6"
+              color="grey-darken-3"
+              rounded
+              variant="flat"
+              text="Leave"
+              height="50"
+              prepend-icon="mdi-arrow-collapse-left"
+              @click="handleAction('leave')"
+              :disabled="!selfIsAtTurn"
+            >
             </v-btn>
           </div>
         </div>
@@ -102,63 +107,123 @@ function handleAction(action) {
       <div class="d-flex flex-column">
         <div class="d-flex justify-center align-center mb-2">
           <div v-for="player in indexedPlayers.slice(0, 2)" :key="player.index">
-            <Player :id="'player-' + player.index" :name="player.player.playername" :balance="player.player.balance"
-              :folded="player.player.folded" :isAtTurn="playerAtTurn === player.index" :position="player.index === 0 ? 'top-left-player' : 'top-right-player'
-
-                " />
+            <Player
+              :id="'player-' + player.index"
+              :name="player.player.playername"
+              :balance="player.player.balance"
+              :folded="player.player.folded"
+              :isAtTurn="playerAtTurn === player.index"
+              :position="
+                player.index === 0 ? 'top-left-player' : 'top-right-player'
+              "
+            />
           </div>
         </div>
 
         <div class="d-flex justify-center align-center">
           <!-- Left, Center, and Right Players and Board -->
-          <div v-for="player in indexedPlayers.slice(3, 4)" :key="player.index" id="player-5">
-            <Player :name="player.player.playername" :balance="player.player.balance" :folded="player.player.folded"
-              position="left-player" :isAtTurn="playerAtTurn === player.index" />
+          <div
+            v-for="player in indexedPlayers.slice(3, 4)"
+            :key="player.index"
+            id="player-5"
+          >
+            <Player
+              :name="player.player.playername"
+              :balance="player.player.balance"
+              :folded="player.player.folded"
+              position="left-player"
+              :isAtTurn="playerAtTurn === player.index"
+            />
           </div>
 
           <div class="d-flex justify-center align-center">
             <div class="table responsive-table">
               <div class="d-flex justify-space-around align-center">
-                <div v-for="player in indexedPlayers.slice(0, 2)" :key="'coins-' + player.index"
-                  class="d-flex justify-center align-center">
+                <div
+                  v-for="player in indexedPlayers.slice(0, 2)"
+                  :key="'coins-' + player.index"
+                  class="d-flex justify-center align-center"
+                >
                   <Coins :amount="player.player.currentAmountBetted" />
-                  <PlayerCards :showCards="player.player.id === getCookie('playerID')" :playerIndex="player.index"
-                    :playerAtTurn="false" :gameState="gameState" />
+                  <PlayerCards
+                    :showCards="player.player.id === getCookie('playerID')"
+                    :playerIndex="player.index"
+                    :playerAtTurn="false"
+                    :gameState="gameState"
+                  />
                 </div>
               </div>
               <div class="d-flex justify-space-between align-center mt-6">
                 <!-- Left Player (if present) -->
-                <div v-if="indexedPlayers[2]" class="d-flex justify-center align-center">
-                  <Coins :amount="indexedPlayers[2].player.currentAmountBetted" />
-                  <PlayerCards :showCards="indexedPlayers[2].player.id === getCookie('playerID')"
-                    :playerIndex="indexedPlayers[2].index" :playerAtTurn="playerAtTurn" :gameState="gameState" />
+                <div
+                  v-if="indexedPlayers[5]"
+                  class="d-flex justify-center align-center"
+                >
+                  <Coins
+                    :amount="indexedPlayers[5].player.currentAmountBetted"
+                  />
+                  <PlayerCards
+                    :showCards="
+                      indexedPlayers[5].player.id === getCookie('playerID')
+                    "
+                    :playerIndex="indexedPlayers[5].index"
+                    :playerAtTurn="playerAtTurn"
+                    :gameState="gameState"
+                  />
                 </div>
 
                 <!-- Pot and Board Cards -->
-                <div class="d-flex flex-column justify-center align-center mx-auto">
+                <div
+                  class="d-flex flex-column justify-center align-center mx-auto"
+                >
                   <div id="pot" class="pot responsive-pot">
                     {{ "$ " + pot }}
                   </div>
-                  <div id="board" class="d-flex justify-center align-center bg-transparent">
-                    <Card v-for="(boardCard, index) in board" :key="index" :rank="boardCard.card.rank.toString()"
-                      :suitID="boardCard.card.suit" />
+                  <div
+                    id="board"
+                    class="d-flex justify-center align-center bg-transparent"
+                  >
+                    <Card
+                      v-for="(boardCard, index) in board"
+                      :key="index"
+                      :rank="boardCard.card.rank.toString()"
+                      :suitID="boardCard.card.suit"
+                    />
                   </div>
                 </div>
 
                 <!-- Right Player (if present) -->
-                <div v-if="indexedPlayers[3]" class="d-flex justify-center align-center">
-                  <Coins :amount="indexedPlayers[3].player.currentAmountBetted" />
-                  <PlayerCards :showCards="indexedPlayers[3].player.id === getCookie('playerID')"
-                    :playerIndex="indexedPlayers[3].index" :playerAtTurn="playerAtTurn" :gameState="gameState" />
+                <div
+                  v-if="indexedPlayers[2]"
+                  class="d-flex justify-center align-center"
+                >
+                  <Coins
+                    :amount="indexedPlayers[2].player.currentAmountBetted"
+                  />
+                  <PlayerCards
+                    :showCards="
+                      indexedPlayers[2].player.id === getCookie('playerID')
+                    "
+                    :playerIndex="indexedPlayers[2].index"
+                    :playerAtTurn="playerAtTurn"
+                    :gameState="gameState"
+                  />
                 </div>
               </div>
 
               <div class="d-flex justify-space-around align-end">
-                <div v-for="player in indexedPlayers.slice(4, 6)" :key="'coins-' + player.index"
-                  class="d-flex justify-center align-center">
+                <div
+                  v-for="player in indexedPlayers.slice(4, 6)"
+                  :key="'coins-' + player.index"
+                  class="d-flex justify-center align-center"
+                >
                   <Coins :amount="player.player.currentAmountBetted" />
-                  <PlayerCards :showCards="player.player.id === getCookie('playerID')" :playerIndex="player.index"
-                    :playerAtTurn="playerAtTurn" :gameState="gameState" />
+                  <PlayerCards
+                    :showCards="player.player.id === getCookie('playerID')"
+                    :playerIndex="player.index"
+                    :playerAtTurn="playerAtTurn"
+                    :gameState="gameState"
+                  />
                 </div>
               </div>
             </div>
@@ -166,53 +231,97 @@ function handleAction(action) {
 
           <!-- Right Player -->
           <div v-if="indexedPlayers.length >= 3" id="player-2">
-            <Player :name="indexedPlayers[2].player.playername" :balance="indexedPlayers[2].player.balance"
-              :folded="indexedPlayers[2].player.folded" position="right-player"
-              :isAtTurn="playerAtTurn === indexedPlayers[2].index" />
+            <Player
+              :name="indexedPlayers[2].player.playername"
+              :balance="indexedPlayers[2].player.balance"
+              :folded="indexedPlayers[2].player.folded"
+              position="right-player"
+              :isAtTurn="playerAtTurn === indexedPlayers[2].index"
+            />
           </div>
         </div>
         <!-- Bottom Players -->
 
         <div class="d-flex justify-center align-center mb-2">
           <div v-for="player in indexedPlayers.slice(4, 6)" :key="player.index">
-            <Player :id="'player-' + player.index" :name="player.player.playername" :balance="player.player.balance"
-              :folded="player.player.folded" :isAtTurn="playerAtTurn === player.index" :position="
-                player.index === 4
-                  ? 'top-left-player'
-                  : 'top-right-player'
-              " />
+            <Player
+              :id="'player-' + player.index"
+              :name="player.player.playername"
+              :balance="player.player.balance"
+              :folded="player.player.folded"
+              :isAtTurn="playerAtTurn === player.index"
+              :position="
+                player.index === 4 ? 'top-left-player' : 'top-right-player'
+              "
+            />
           </div>
         </div>
       </div>
       <!-- Action Buttons -->
-      <div class="container-fluid w-100 ">
+      <div class="container-fluid w-100">
         <div class="d-flex align-center justify-end row">
           <div class="col d-flex gap-3 align-end">
-            <v-btn class="responsive-button mr-4 font-weight-bold text-none text-h6" color="red-darken-4" rounded
-              variant="flat" text="Fold" height="50" prepend-icon="mdi-close" @click="handleAction('fold')"
-              :disabled="!selfIsAtTurn">
+            <v-btn
+              class="responsive-button mr-4 font-weight-bold text-none text-h6"
+              color="red-darken-4"
+              rounded
+              variant="flat"
+              text="Fold"
+              height="50"
+              prepend-icon="mdi-close"
+              @click="handleAction('fold')"
+              :disabled="!selfIsAtTurn"
+            >
             </v-btn>
-            <v-btn class="responsive-button mr-4 font-weight-bold text-none text-h6" color="green-darken-4" rounded
-              variant="flat" :text="callCheckButtonText" height="50" prepend-icon="mdi-check"
-              @click="handleAction('callCheck')" :disabled="!selfIsAtTurn">
+            <v-btn
+              class="responsive-button mr-4 font-weight-bold text-none text-h6"
+              color="green-darken-4"
+              rounded
+              variant="flat"
+              :text="callCheckButtonText"
+              height="50"
+              prepend-icon="mdi-check"
+              @click="handleAction('callCheck')"
+              :disabled="!selfIsAtTurn"
+            >
             </v-btn>
             <div class="d-flex flex-column">
-              <div class="col d-flex justify-end text-white mr-4 mb-4 font text-h5" id="sliderValue">
+              <div
+                class="col d-flex justify-end text-white mr-4 mb-4 font text-h5"
+                id="sliderValue"
+              >
                 ${{ sliderValue }}
               </div>
-              <v-btn class="responsive-button font-weight-bold text-none text-h6" color="blue-darken-4" rounded
-                variant="flat" text="Raise" height="50" prepend-icon="mdi-arrow-up-thin" @click="handleAction('raise')"
-                :disabled="!selfIsAtTurn">
+              <v-btn
+                class="responsive-button font-weight-bold text-none text-h6"
+                color="blue-darken-4"
+                rounded
+                variant="flat"
+                text="Raise"
+                height="50"
+                prepend-icon="mdi-arrow-up-thin"
+                @click="handleAction('raise')"
+                :disabled="!selfIsAtTurn"
+              >
               </v-btn>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="d-flex flex-row justify-center align-end mb-4" style="width: 100px;">
-      <v-slider class="mr-5 responsive-slider-size" v-model="sliderValue" min="0" max="1000" step="10" color="primary"
-        direction="vertical" />
-
+    <div
+      class="d-flex flex-row justify-center align-end mb-4"
+      style="width: 100px"
+    >
+      <v-slider
+        class="mr-5 responsive-slider-size"
+        v-model="sliderValue"
+        min="0"
+        max="1000"
+        step="10"
+        color="primary"
+        direction="vertical"
+      />
     </div>
   </div>
 </template>
